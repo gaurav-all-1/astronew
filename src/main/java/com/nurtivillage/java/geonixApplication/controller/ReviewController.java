ffb79fee6f82c57010ec012b2f600adf5b6b3895
Anurag Pundir <anurag.pundir@ihsmarkit.com>
2023-03-22 15:39:56 +0530

    change

--- src/main/java/com/nurtivillage/java/geonixApplication/controller/ReviewController.java
+++ src/main/java/com/nurtivillage/java/geonixApplication/controller/ReviewController.java
@@ -50,11 +50,10 @@ public class ReviewController {
     public String testsms(){
         String s = "sent";
         try {
-            smsService.sendSms(123,"9953700301");
+            smsService.sendSms("MAR03222023/3331","9953700301");
         } catch (Exception e) {
             s=e.getMessage();
             e.printStackTrace();
-//            return new ResponseEntity<String>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return s;
     }
