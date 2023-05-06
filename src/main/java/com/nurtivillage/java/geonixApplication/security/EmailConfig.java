16c0f234a20ee76ab8e473b0c1b7a50f6d4e31c1
gaurav-all-1 <gauravsinghal5678gs678@gmail.com>
2023-05-06 13:57:39 +0530

    change

---
+++ src/main/java/com/nurtivillage/java/geonixApplication/security/EmailConfig.java
@@ -0,0 +1,28 @@
+package com.nurtivillage.java.geonixApplication.security;
+
+import java.util.Properties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class EmailConfig {
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSender mailSender = new JavaMailSenderImpl();
+        ((JavaMailSenderImpl) mailSender).setHost("smtp.gmail.com");
+        ((JavaMailSenderImpl) mailSender).setPort(25);
+
+        ((JavaMailSenderImpl) mailSender).setUsername("geonixindiaonline@gmail.com");
+        ((JavaMailSenderImpl) mailSender).setPassword("nlkrcusfhsqmqkxr");
+
+        Properties props = ((JavaMailSenderImpl) mailSender).getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        return mailSender;
+    }
+}
